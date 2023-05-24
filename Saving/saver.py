07e79c819aaef4ecefb637a7a764da6a60ae42a4
Vincent Uber <30577533+vinhero@users.noreply.github.com>
2023-05-24 09:02:33 +0200

    saver generated

---
+++ Saving/saver.py
@@ -0,0 +1,9 @@
+class Saver:
+    def __init__(self) -> None:
+        pass
+
+    def save(self) -> None:
+        pass
+
+    def load(self) -> None:
+        pass
---
+++ Saving/sqlsaver.py
@@ -0,0 +1,13 @@
+import saver
+
+
+class SQLSaver(saver):
+
+    def __init__(self):
+        pass
+
+    def save(self):
+        pass
+
+    def load(self):
+        pass
